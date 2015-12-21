2acd0c923d178e5e067a57a0a122bc29a077f910
Vongola <vongola12324@coder.tw>
2015-12-21 09:54:59 +0800

    Add Question

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 
 public class Main {
     public static void main(String[] args){
-        TestPaper Paper = new TestPaper();
-        Paper.addQuestion(new Question("1+1=?"));
+        Exam Paper = new TestPaper();
+        if(Paper instanceof TestPaper){
+            Paper.addQuestion(new Question("1 + 1 = ?"));
+            Paper.addQuestion(new Question("2 + 2 = ?"));
+            Paper.addQuestion(new Question("3 + 3 = ?"));
+            Paper.addQuestion(new Question("10 + 10 = ?"));
+            Paper.addQuestion(new Question("14023984028 + 17749273498720482 = ?"));
+        }
+        Paper.doQuestion();
+
     }
 }
